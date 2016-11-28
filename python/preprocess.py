d1cff351a0b2cabd3ca780ae0d9b255970deccc6
paulmeinz <paulmeinz@gmail.com>
2016-11-27 20:13:31 -0800

    feat: removed some text characters that were not being interpreted proberly and split text strings

--- python/preprocess.py
+++ python/preprocess.py
@@ -1,11 +1,13 @@
 import re
 import docx
 import os
+import string
 
 
 files = os.listdir("/home/paul/Documents/Code/NeuralAAI/python/files/")
 
 pat = re.compile(".docx")
+slt = re.compile("[ ,.-;:$&!]")
 
 # Grab the files with the docx extension
 
@@ -18,8 +20,14 @@ for i in files:
 # Read the files
 
 for i in docs:
+    text = []
     doc = docx.Document('files/' + i)
     for j in doc.paragraphs:
         for k in j.runs:
-            print k.bold, k.italic, k.underline # Determine fonts
+            if k.bold == None and k.italic == None and k.underline == None:
+                x = k.text
+                x = x.replace(u"\u2019", "").replace(u"\u2013", "").replace(u"\u2018", "")
+                x = slt.split(x)
+                text.extend(x)
+    print(text)
 
