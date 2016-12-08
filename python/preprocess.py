3f3efa87ce9fe6eecb96c253a9becb342945ce49
paulmeinz <paulmeinz@gmail.com>
2016-12-07 21:54:16 -0800

    feat: replaced unicode characters in text

--- python/preprocess.py
+++ python/preprocess.py
@@ -8,9 +8,12 @@ import csv
 files = os.listdir("/home/paul/Documents/Code/NeuralAAI/python/files/")
 
 pat = re.compile(".doc")
-slt = re.compile('''[-&$#/~`?;:<> ,}{.-;:$&!)(]|\]|\[''')
+slt = re.compile('''[-&$#/~`?;:<> ,}{.-;:$&!)("]|\]|\[''')
 grp = re.compile("\(.*?\)|\{.*?\}|\[.*?\]")
-alt = re.compile(u"\u2019|\u2018|\u201c|\u201d|\u2013|\u2026|\u2014|\u2026")
+squote = re.compile(u"\u2019|\u2018")
+dquote = re.compile(u"\u201c|\u201d")
+dash = re.compile(u"\u2013|\u2014")
+elipse = re.compile(u"\u2026")
 
 mydict = {}
 
@@ -32,10 +35,15 @@ for i in docs:
             if k.bold == None and k.italic == None and k.underline == None:
                 x = k.text
                 text += x
-    text = re.sub(alt, "", text)
+    text = re.sub(squote, "'", text)
+    text = re.sub(dquote, '''"''', text)
+    text = re.sub(dash, "-", text)
+    text = re.sub(elipse, "...", text)
     text = re.sub(grp, "", text)
     text = slt.split(text)
     newtext = [p for p in text if p != '']     
     mydict[i] = newtext
+
+print(mydict[i])
 
 
